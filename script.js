38f07ec5402a83289408d53bdbedc88b7688620d
argreality <65363004+argreality@users.noreply.github.com>
2020-09-02 21:30:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script>
  function isFacebookApp() {
         var ua = navigator.userAgent || navigator.vendor || window.opera;
         return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
@@ -7,4 +6,4 @@
          // your action here if user using Facebook in-app browser
         alert('Are you using Facebook Browser? To get better experience, try press [...] and Open the AR in Chrome or Safari. Thank you!');
     }
-</script>
+
